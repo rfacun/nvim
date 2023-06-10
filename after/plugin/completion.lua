743e53c67f73d4e34c465ddbfe7e9f6aa5523216
rfacun <rossi_facundo@outlook.com>
2023-06-10 14:57:33 -0300

    modified some mappings

--- after/plugin/completion.lua
+++ after/plugin/completion.lua
@@ -36,11 +36,12 @@ cmp.setup({
 
    -- Mappings
    mapping = cmp.mapping.preset.insert({
-      ['<C-p>'] = cmp.mapping.scroll_docs(-5),
-      ['<C-n>'] = cmp.mapping.scroll_docs(5),
+      ['<C-h>'] = cmp.mapping.scroll_docs(-5),
+      ['<C-l>'] = cmp.mapping.scroll_docs(5),
       ["<C-k>"] = cmp.mapping.select_prev_item(),
       ["<C-j>"] = cmp.mapping.select_next_item(),
       ['<CR>'] = cmp.mapping.confirm({ select = false }),
+      ['<C-e>'] = cmp.mapping.abort(),
 
       -- Super tab (next field)
       ["<Tab>"] = cmp.mapping(function(fallback)
--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,7 +1,7 @@
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
-vim.keymap.set("n", "<leader>a", mark.add_file)
+vim.keymap.set("n", "<leader>af", mark.add_file)
 vim.keymap.set("n", "<leader>aj", ui.toggle_quick_menu)
 
 vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
